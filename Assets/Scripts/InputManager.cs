3dc4b1add769543c3a6353d36dd83f458e0086ed
sabanisi <taisuke140909@gmail.com>
2023-07-06 02:53:33 +0900

    短冊作成

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    /// <summary>
+    /// 入力を受け取るクラス
+    /// </summary>
+    public class InputManager:MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField textInput;
+        [SerializeField] private TMP_InputField nameInput;
+        
+        public void Decide()
+        {
+            
+        }
+
+        public void Cancel()
+        {
+            
+        }
+    }
+}
